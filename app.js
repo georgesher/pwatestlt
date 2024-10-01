5f0a8fe2abd47a2ddf3b10f199008205f0f31a0b
georgesher <71032370+georgesher@users.noreply.github.com>
2024-10-01 10:34:47 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 // Инициализация Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBRuZatY8-UVZjIx5f01BIYiTe2c7441so",
-  authDomain: "teststl24.firebaseapp.com",
-  projectId: "teststl24",
-  storageBucket: "teststl24.appspot.com",
-  messagingSenderId: "601498787082",
-  appId: "1:601498787082:web:0ff88474a5e0a36774eef3"
+    apiKey: "AIzaSyBRuZatY8-UVZjIx5f01BIYiTe2c7441so",
+    authDomain: "teststl24.firebaseapp.com",
+    projectId: "teststl24",
+    storageBucket: "teststl24.appspot.com",
+    messagingSenderId: "601498787082",
+    appId: "1:601498787082:web:0ff88474a5e0a36774eef3"
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -13,7 +13,7 @@ firebase.initializeApp(firebaseConfig);
 // Получение объекта для работы с Firebase Messaging
 const messaging = firebase.messaging();
 
-navigator.serviceWorker.register('firebase-messaging-sw.js')
+navigator.serviceWorker.register('./firebase-messaging-sw.js')
 .then(function(registration) {
     console.log('Service Worker Registered:', registration);
     messaging.useServiceWorker(registration);
@@ -23,7 +23,7 @@ navigator.serviceWorker.register('firebase-messaging-sw.js')
 document.getElementById('subscribe').addEventListener('click', function() {
     Notification.requestPermission().then(function(permission) {
         if (permission === 'granted') {
-            messaging.getToken({ vapidKey: 'YOUR_VAPID_KEY' }).then(function(currentToken) {
+            messaging.getToken({ vapidKey: 'YOUR_PUBLIC_VAPID_KEY' }).then(function(currentToken) {
                 if (currentToken) {
                     console.log('FCM токен:', currentToken);
                     alert('Subscribed to Push Notifications!');
