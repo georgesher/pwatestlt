b15d8a5106f5a8b41556c0ac37536cd645f50119
georgesher <71032370+georgesher@users.noreply.github.com>
2024-10-01 10:35:01 -0300

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -4,22 +4,22 @@ importScripts('https://www.gstatic.com/firebasejs/9.15.0/firebase-messaging.js')
 // Инициализация Firebase
 firebase.initializeApp({
     apiKey: "AIzaSyBRuZatY8-UVZjIx5f01BIYiTe2c7441so",
-  authDomain: "teststl24.firebaseapp.com",
-  projectId: "teststl24",
-  storageBucket: "teststl24.appspot.com",
-  messagingSenderId: "601498787082",
-  appId: "1:601498787082:web:0ff88474a5e0a36774eef3"
+    authDomain: "teststl24.firebaseapp.com",
+    projectId: "teststl24",
+    storageBucket: "teststl24.appspot.com",
+    messagingSenderId: "601498787082",
+    appId: "1:601498787082:web:0ff88474a5e0a36774eef3"
 });
 
 const messaging = firebase.messaging();
 
-// Обработка входящих push-уведомлений
+// Обработка входящих push-уведомлений в фоновом режиме
 messaging.onBackgroundMessage(function(payload) {
     console.log('Received background message ', payload);
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: '/icon.png'
+        icon: 'icon.png'
     };
 
     self.registration.showNotification(notificationTitle, notificationOptions);
